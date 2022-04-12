adab259d70478b92d044a5dd315d0436583f5ab7
wudiiii <wudi9872@yeah.net>
2022-04-12 13:10:24 +0800

    finish init function

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,7 +2,14 @@ package gitlet;
 
 // TODO: any imports you need here
 
+import java.io.File;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Date; // TODO: You'll likely use this in this class
+import java.util.TreeMap;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
@@ -10,7 +17,7 @@ import java.util.Date; // TODO: You'll likely use this in this class
  *
  *  @author TODO
  */
-public class Commit {
+public class Commit implements Serializable {
     /**
      * TODO: add instance variables here.
      *
@@ -22,5 +29,86 @@ public class Commit {
     /** The message of this Commit. */
     private String message;
 
+    /**
+     * The creation time of this commit
+     */
+    private Date date;
+
+    /**
+     * Mappings from file names to their corresponding blobs (SHA-1 value)
+     */
+    private TreeMap<String, String> fileRefs;
+
+    /**
+     * The main parent commit of this commit
+     */
+    private String parent;
+
+    /**
+     * The second parent commit of this commit (for merging)
+     */
+    private String sParent;
+
     /* TODO: fill in the rest of this class. */
+
+    /**
+     * This function is for initial commit
+     */
+    public Commit(String message) {
+        this.date = new Date();
+        this.message = message;
+    }
+
+    /**
+     * This function is for commit with one parent
+     */
+    public Commit(String message, TreeMap<String, String> newFileRefs, String parent) {
+        this.date = new Date();
+        this.message = message;
+
+        // deal with file references
+        this.fileRefs = new TreeMap<>();
+        inheritRefs(parent);
+        this.fileRefs.putAll(newFileRefs);
+
+        this.parent = parent;
+    }
+
+    /**
+     * inherit file references from parent commit
+     * @param parent the hash value of parent commit
+     */
+    private void inheritRefs(String parent) {
+        File parentFile = Utils.join(Repository.COMMITS_DIR, parent);
+        Commit parentCommit = Utils.readObject(parentFile, Commit.class);
+        // if the parent commit is the initial commit (which means that the file reference map is null)
+        if (parentCommit.fileRefs == null) {
+            return;
+        }
+        this.fileRefs.putAll(parentCommit.fileRefs);
+    }
+
+    /**
+     * This function is for commit with two parents
+     */
+    public Commit(String message, TreeMap<String, String> fileRefs, String parent, String sParent) {
+        this.date = new Date();
+        this.message = message;
+        //TODO add inherit reference function for two parents
+        this.fileRefs = fileRefs;
+        this.parent = parent;
+        this.sParent = sParent;
+    }
+
+    public void makePersistent() {
+        File commitFile = Utils.join(Repository.COMMITS_DIR, hash());
+        Utils.writeObject(commitFile, this);
+    }
+
+    /**
+     * get the string representation of SHA-1 hash value of this commit
+     */
+    public String hash() {
+        return Utils.sha1(Utils.serialize(this));
+    }
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,7 @@
 package gitlet;
 
 import java.io.File;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -25,5 +26,124 @@ public class Repository {
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
+    /**
+     * The default name of the master branch
+     */
+    public static final String masterBranchName = "master";
+
+    /**
+     * The blobs directory
+     */
+    public static final File BLOBS_DIR = join(GITLET_DIR, "blobs");
+
+    /**
+     * The commits directory
+     */
+    public static final File COMMITS_DIR = join(GITLET_DIR, "commits");
+
+    /**
+     * The heads directory
+     */
+    public static final File HEADS_DIR = join(GITLET_DIR, "heads");
+
+    /**
+     * The current head file
+     */
+    public static final File CUR_HEAD = join(GITLET_DIR, "HEAD");
+
+
     /* TODO: fill in the rest of this class. */
+    public static void init() {
+        if (GITLET_DIR.exists()) {
+            System.out.println("A gitlet version-control system already exists in the current directory.");
+            System.exit(0);
+        } else {
+            initDirs();
+            // create initial commit
+            Commit initial_commit = new Commit("initial commit");
+            initial_commit.makePersistent();
+            String hashText = initial_commit.hash();
+
+            checkoutCommit(hashText);
+            // initialize the master branch
+            createBranch(masterBranchName);
+
+            // set current head to current branch reference
+            checkoutBranch(masterBranchName);
+
+            System.out.println("gitlet initialization success!");
+        }
+    }
+
+    /**
+     * Initialize all the directories in .gitlet
+     */
+    private static void initDirs() {
+        GITLET_DIR.mkdir();
+        BLOBS_DIR.mkdir();
+        COMMITS_DIR.mkdir();
+        HEADS_DIR.mkdir();
+    }
+
+    /**
+     * Check out a commit based on its hash value
+     * @param hashText the hash value of the commit we want to check out
+     */
+    public static void checkoutCommit(String hashText) {
+        Utils.writeContents(CUR_HEAD, hashText);
+    }
+
+    /**
+     * Check out a branch reference based on given branch name
+     * @param branchName the given branch name
+     */
+    public static void checkoutBranch(String branchName) {
+        // write the branch name to current head
+        Utils.writeContents(CUR_HEAD, "ref: ", branchName);
+    }
+
+    /**
+     * Create a branch from current HEAD based on given branch name
+     * @param BranchName The name of the branch to be created
+     */
+    public static void createBranch(String BranchName) {
+        File branchFile = join(HEADS_DIR, BranchName);
+        String headCommitHash = parseHeadToHash();
+        Utils.writeContents(branchFile, headCommitHash);
+    }
+
+    private static String parseHeadToHash() {
+        if (isRef(CUR_HEAD)) {
+            return readRefToHash(CUR_HEAD);
+        } else {
+            return readContentsAsString(CUR_HEAD);
+        }
+    }
+
+    /**
+     * Read the file to commit hash value
+     * @param head The file to be read
+     * @return The hash value of the commit this head points to
+     */
+    private static String readRefToHash(File head) {
+        String refForm = readContentsAsString(head);
+        String[] split = refForm.split(" ");
+        String refTag = split[0];
+        String branchName = split[1];
+        File branchFile = join(HEADS_DIR, branchName);
+        return readContentsAsString(branchFile);
+    }
+
+    /**
+     * check if a file contains branch reference information
+     * @param head the file to be checked
+     * @return true if it is a branch reference
+     */
+    private static boolean isRef(File head) {
+        String refForm = readContentsAsString(head);
+        if (refForm.contains("ref")) {
+            return true;
+        }
+        return false;
+    }
 }
