07745e87f34e223bcda4f0cdd41bffddc8a64cd3
wudi <cs.wudilet@gmail.com>
2023-04-14 23:45:04 +0800

    init saver class

---
+++ proj3/byow/Core/GameSaver.java
@@ -0,0 +1,21 @@
+package byow.Core;
+
+import java.util.*;
+import java.io.*;
+
+public class GameSaver {
+	public static final String dir = System.getProperty("user.dir");
+
+	// TETile[][] world;
+
+	// public void save() {
+	// 	FileWriter fw = new FileWriter("record.txt");
+	// 	try {
+	// 		if (saveFile.createNewFile()) {
+	// 			System.out.println();
+	// 		}
+	// 	} catch () {
+
+	// 	}
+	// }
+}
