5cddd22cb0daa43377b4a0c4e7b2402945e1e232
wudi <cs.wudilet@gmail.com>
2023-04-14 19:55:58 +0800

    input string case insensitive

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -163,7 +163,8 @@ public class Engine {
         //
         // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
         // that works for many different input types.
-		if (input.charAt(0) != 'N' || input.charAt(input.length() - 1) != 'S') {
+		if (input.charAt(0) != 'N' || input.charAt(input.length() - 1) != 'S'
+			|| input.charAt(0) != 'n' || input.charAt(0) != 's') {
 			throw new RuntimeException("The input string is not valid");
 		}
 		String numberStr = input.substring(1, input.length() - 1);
